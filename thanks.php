b6ad6c7e5612b347633bdb9a76c426bf3cefc6ee
okada kochi <kochi51@kooks-MacBook-Air.local>
2024-02-27 22:52:03 +0900

    確認画面ちょっと変更しました

--- thanks.php
+++ thanks.php
@@ -4,8 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>送信ありがとうございます</title>
+    <link rel="icon" href="./img/favicon.ico">
+    <link rel="stylesheet" href="slick/slick.css">
+    <link rel="stylesheet" href="slick/slick-theme.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
+    <header class="header">
+        <div class="header_logo">
+          <h1 class="header_logoText">Sneakers</h1>
+        </div>
+        <div class="header_hamburger">
+          <div class="header_hamburgerLineArea">
+            <span class="header_hamburgerLine"></span>
+            <span class="header_hamburgerLine"></span>
+            <span class="header_hamburgerLine"></span>
+          </div>
+        </div>
+        <nav class="header_menu">
+          <ul class="header_menuArea">
+            <li class="header_menuItem"><a class="header_menuItemLink" href="#pickUp">PICK UP</a></li>
+            <li class="header_menuItem"><a class="header_menuItemLink" href="#feature">FEATURE</a></li>
+            <li class="header_menuItem"><a class="header_menuItemLink" href="#contact">CONTACT</a></li>
+            <li class="header_menuItem"><a class="header_menuItemLink" href="https://twitter.com/?lang=ja"
+                target="_blank" rel="noopener noreferrer">Twitter</a>
+            </li>
+            <li class="header_menuItem"><a class="header_menuItemLink" href="https://www.facebook.com/?locale=ja_JP"
+                target="_blank" rel="noopener noreferrer">Facebook</a></li>
+            <li class="header_menuItem"><a class="header_menuItemLink" href="https://www.instagram.com/" target="_blank"
+                rel="noopener noreferrer">Instagram</a></li>
+          </ul>
+        </nav>
+    </header>
     <section id="thanks">
         <form action="index.html" method="post">
             <h2>お問い合わせ送信完了</h2>
@@ -27,5 +57,9 @@
             ?>
         </form>
     </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="slick/slick.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
