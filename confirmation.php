52bb887218ffa8fff68545545b8fe910150369b2
okada kochi <kochi51@kooks-MacBook-Air.local>
2024-02-28 23:05:20 +0900

    確認画面ちょっと変えた

--- confirmation.php
+++ confirmation.php
@@ -7,54 +7,56 @@
     <title>確認</title>
 </head>
 <body>
-<section id="confirmation">
-    <h1 class="title">お問い合わせ内容</h1>
-    <div class="confirmation-info">
-        <p>お問い合わせ内容はこちらで宜しいでしょうか？<br>
-           よろしければ「送信する」ボタンを押して下さい。
-        </p>
-    </div>
+<div class="fullscreen">
+    <section id="confirmation">
+        <h1 class="title">お問い合わせ内容</h1>
+        <div class="confirmation-info">
+            <p>お問い合わせ内容はこちらで宜しいでしょうか？<br>
+            よろしければ「送信する」ボタンを押して下さい。
+            </p>
+        </div>
 
-    <div class="confirmation-form">
-        <form action="thanks.php" method="post">
-            <div class="contact-group">
-                <ul class="contact-list">
-                    <li><h5>Name: </h5></li>
-                    <li><?php echo isset($_POST["username"]) ? htmlspecialchars($_POST["username"]) : "" ?></li>
-                </ul>
-                <ul class="contact-list">
-                    <li><h5>Mail: </h5></li>
-                    <li><?php echo isset($_POST["useremail"]) ? htmlspecialchars($_POST["useremail"]) : "" ?></li>
-                </ul>
-                <ul class="contact-list">
-                    <li><h5>Message: </h5></li>
-                    <li><?php echo isset($_POST["inquiry"]) ? nl2br(htmlspecialchars($_POST["inquiry"])) : "" ?></li>
-                </ul>
-            </div>
+        <div class="confirmation-form">
+            <form action="thanks.php" method="post">
+                <div class="contact-group">
+                    <ul class="contact-list">
+                        <li><h5>Name: </h5></li>
+                        <li><?php echo isset($_POST["username"]) ? htmlspecialchars($_POST["username"]) : "" ?></li>
+                    </ul>
+                    <ul class="contact-list">
+                        <li><h5>Mail: </h5></li>
+                        <li><?php echo isset($_POST["useremail"]) ? htmlspecialchars($_POST["useremail"]) : "" ?></li>
+                    </ul>
+                    <ul class="contact-list">
+                        <li><h5>Message: </h5></li>
+                        <li><?php echo isset($_POST["inquiry"]) ? nl2br(htmlspecialchars($_POST["inquiry"])) : "" ?></li>
+                    </ul>
+                </div>
 
-            <!-- <div class="data">
-                <dl>
-                    <dt>Name: </dt>
-                    <dd><?php echo isset($_POST["username"]) ? htmlspecialchars($_POST["username"]) : "" ?></dd>
-                </dl>
-                <dl>
-                    <dt>Mail: </dt>
-                    <dd><?php echo isset($_POST["useremail"]) ? htmlspecialchars($_POST["useremail"]) : "" ?></dd>
-                </dl>
-                <dl>
-                    <dt>Message: </dt>
-                    <dd><?php echo isset($_POST["inquiry"]) ? htmlspecialchars($_POST["inquiry"]) : "" ?></dd>
-                </dl>
-            </div> -->
+                <!-- <div class="data">
+                    <dl>
+                        <dt>Name: </dt>
+                        <dd><?php echo isset($_POST["username"]) ? htmlspecialchars($_POST["username"]) : "" ?></dd>
+                    </dl>
+                    <dl>
+                        <dt>Mail: </dt>
+                        <dd><?php echo isset($_POST["useremail"]) ? htmlspecialchars($_POST["useremail"]) : "" ?></dd>
+                    </dl>
+                    <dl>
+                        <dt>Message: </dt>
+                        <dd><?php echo isset($_POST["inquiry"]) ? htmlspecialchars($_POST["inquiry"]) : "" ?></dd>
+                    </dl>
+                </div> -->
 
-            <div class="submit">
-                <!--戻るを押すと前の画面に遷移-->
-                <button type="button" onclick="history.back(-1)">戻る</button><button>送信</button>
-            </div>
+                <div class="submit">
+                    <!--戻るを押すと前の画面に遷移-->
+                    <button type="button" onclick="history.back(-1)">戻る</button><button>送信</button>
+                </div>
 
-            <!-- <input type="submit" value="送信" /> -->
-        </form>
-    </div>
-</section>
+                <!-- <input type="submit" value="送信" /> -->
+            </form>
+        </div>
+    </section>
+</div>
 </body>
 </html>
--- thanks.php
+++ thanks.php
@@ -38,26 +38,27 @@
     </header>
     <section id="thanks">
         <form action="index.html" method="post">
-            <h2>お問い合わせ送信完了</h2>
-
-            <p class="message">
-                お問い合わせありがとうございます。1営業日以内にご返信させていただきます。
-            </p>
-            
-            <button type="submit" name="topback">TOPへ戻る</button>
-            <!--topへ戻るボタンを押すと入力していた値を消してtopへ戻る処理-->
-            <?php
-                if(isset($_POST["topback"])){
-                    $_POST["username"] = "";
-                    $_POST["useremail"] = "";
-                    $_POST["inquiry"] = "";
-                    header("Location: index.html");
-                    exit();
-                }
-            ?>
+            <div class="mask-bg anim">
+                <div class="thanks-form">
+                    <h2>お問い合わせ送信完了</h2>
+                    <p class="message">
+                        お問い合わせありがとうございます。1営業日以内にご返信させていただきます。
+                    </p>
+                    <button type="submit" name="topback">TOPへ戻る</button>
+                    <!--topへ戻るボタンを押すと入力していた値を消してtopへ戻る処理-->
+                    <?php
+                        if(isset($_POST["topback"])){
+                            $_POST["username"] = "";
+                            $_POST["useremail"] = "";
+                            $_POST["inquiry"] = "";
+                            header("Location: index.html");
+                            exit();
+                        }
+                    ?>
+                </div>
+            </div>
         </form>
     </section>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="slick/slick.min.js"></script>
     <script src="js/script.js"></script>
